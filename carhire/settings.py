f08c8771523d618e8560edc74c56a075fc346dcc
Marymagdaline Atsieno <marymagdaline>
2025-08-08 21:13:32 +0300

    'changes'

--- carhire/settings.py
+++ carhire/settings.py
@@ -129,6 +129,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'maryokutse@gmail.com'
+EMAIL_HOST_USER = 'benangano@gmail.com'
 EMAIL_HOST_PASSWORD = 'xncl hqcx femt hsge'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
